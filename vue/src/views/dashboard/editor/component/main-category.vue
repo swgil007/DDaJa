c36dee502d701c5743034444a3cacec335c2019c
soobinJung <soobin7999@naver.com>
2021-04-29 21:52:16 +0900

    #12 License Image Mapping Success!!! v 0_0 v

--- vue/src/views/dashboard/editor/component/main-category.vue
+++ vue/src/views/dashboard/editor/component/main-category.vue
@@ -1,71 +1,109 @@
 <template>
-    <div class="main-category-div">
-      <div class="category-div">
-        <div><router-link to="/big-data-gisa/index"><img v-bind:src="grayMan" class="mans-img" ></router-link></div>
-        <div><router-link to="/"><img v-bind:src="blueMan" class="mans-img" ></router-link></div>
-        <div><router-link to="/network-admin-one/index">네1<img v-bind:src="blueMan" class="mans-img" ></router-link></div>
-        <div><router-link to="/network-admin-two/index">네2<img v-bind:src="blueMan" class="mans-img" ></router-link></div>
+  <div class="main-category-div">
+    <div class="category-div">
+      <div>
+        <router-link to="/jeongbo-cheoli-sanup-gisa/index">
+          <img
+            :src="LOGO_JCSG"
+            class="mans-img"
+            @mouseenter="LOGO_JCSG = JCSG_H"
+            @mouseleave="LOGO_JCSG = JCSG"
+          >
+        </router-link>
       </div>
-      <div class="category-div">
-        <div><router-link to="/jeongbo-cheoli-gisa/index"><img v-bind:src="redMan" class="mans-img" ></router-link></div>
-        <div><router-link to="/jeongbo-cheoli-sanup-gisa/index">산업기사<img v-bind:src="purpleMan" class="mans-img" ></router-link></div>
-        <div><router-link to="/computer-utilize-one/index"><img v-bind:src="purpleMan" class="mans-img" ></router-link></div>
- <div><router-link to="/computer-utilize-two/index"><img v-bind:src="purpleMan" class="mans-img" ></router-link></div>
-      </div> 
+      <div>
+        <router-link to="/jeongbo-cheoli-gisa/index">
+          <img
+            :src="LOGO_JCG"
+            class="mans-img"
+            @mouseenter="LOGO_JCG = JCG_H"
+            @mouseleave="LOGO_JCG = JCG"
+          >
+          >
+        </router-link>
+      </div>
+      <div>
+        <router-link to="/computer-utilize-one/index">
+          <img
+            :src="LOGO_CUO"
+            class="mans-img"
+            @mouseenter="LOGO_CUO = CUO_H"
+            @mouseleave="LOGO_CUO = CUO"
+          >
+        </router-link>
+      </div>
+      <div>
+        <router-link to="/computer-utilize-two/index">
+          <img
+            :src="LOGO_CUT"
+            class="mans-img"
+            @mouseenter="LOGO_CUT = CUT_H"
+            @mouseleave="LOGO_CUT = CUT"
+          >
+        </router-link>
+      </div>
     </div>
+  </div>
 </template>
-
 
 <script>
 
-import redMan from '@/assets/character/red_man.png'
-import blueMan from '@/assets/character/blue_man.png'
-import purpleMan from '@/assets/character/purple_man.png'
-import grayMan from '@/assets/character/gray_man.png'
+import CUO from '@/assets/mascot/computer-utilize-one.png'
+import CUT from '@/assets/mascot/computer-utilize-two.png'
+import JCG from '@/assets/mascot/jeongbo-cheoli-gisa.png'
+import JCSG from '@/assets/mascot/jeongbo-cheoli-sanup-gisa.png'
+
+import CUO_H from '@/assets/mascot/computer-utilize-one-hover.png'
+import CUT_H from '@/assets/mascot/computer-utilize-two-hover.png'
+import JCG_H from '@/assets/mascot/jeongbo-cheoli-gisa-hover.png'
+import JCSG_H from '@/assets/mascot/jeongbo-cheoli-sanup-gisa-hover.png'
 
 export default {
+  components: {
+
+  },
   data() {
     return {
-      redMan: redMan,
-      blueMan : blueMan,
-      purpleMan : purpleMan,
-      grayMan :grayMan
-    };
-  },
-  components :{
-    
+      LOGO_CUO: CUO,
+      LOGO_CUT: CUT,
+      LOGO_JCG: JCG,
+      LOGO_JCSG: JCSG,
+      CUO_H: CUO_H,
+      CUT_H: CUT_H,
+      JCG_H: JCG_H,
+      JCSG_H: JCSG_H,
+      CUO: CUO,
+      CUT: CUT,
+      JCG: JCG,
+      JCSG: JCSG
+    }
   }
-};
+}
 </script>
 
-<style lang="scss" scoped> 
+<style lang="scss" scoped>
 
 * {
   background-color: rgb(11, 8, 49);
 }
 
-.main-category-div {   
-  
-  .category-div {  
-    padding-right: 5.8%; 
+.main-category-div {
+  padding: 300px 0 500px 0;
+  .category-div {
+    padding-right: 5.8%;
     padding-left: 5.8%;
-
     max-width: 100%;
-    float: left; 
-    height: 100%;  
-     
-    div { 
+    float: left;
+    height: 100%;
+    div {
       width: 25%;
       height: 25%;
       float: left;
     }
   }
 }
- 
-
 .mans-img{
   width: 420px;
   height: 420px;
-
 }
 </style>
