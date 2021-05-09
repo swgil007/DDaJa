470249f0290af7ec0d9af366ef4138805778f340
soobinJung <soobin7999@naver.com>
2021-05-09 18:16:54 +0900

    [#5] session setting

--- vue/src/main.js
+++ vue/src/main.js
@@ -13,6 +13,7 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import store from './store'
 import router from './router'
+import VueSession from 'vue-session'
 
 import './icons' // icon
 import './permission' // permission control
@@ -20,14 +21,11 @@ import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
 
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online ! ! !
- */
+var sessionOptions = {
+  persist: true
+}
+Vue.use(VueSession, sessionOptions)
+
 if (process.env.NODE_ENV === 'production') {
   const { mockXHR } = require('../mock')
   mockXHR()
