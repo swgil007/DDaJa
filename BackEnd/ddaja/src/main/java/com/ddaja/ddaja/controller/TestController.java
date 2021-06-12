7c4b34ddb0200758b1376d2fe3e6d39846f34655
swgil007 <swgil007@naver.com>
2021-06-12 14:14:56 +0900

    [#19] change structure controller

--- BackEnd/ddaja/src/main/java/com/ddaja/ddaja/controller/TestController.java
+++ BackEnd/ddaja/src/main/java/com/ddaja/ddaja/controller/TestController.java
@@ -1,6 +1,7 @@
-package com.ddaja.controller;
+package com.ddaja.ddaja.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestController {
     
     @RequestMapping("/")
+    @GetMapping
     @ResponseBody
     public String home() {
         return "This Is Just Home";
+    }
+
+    @RequestMapping("/test")
+    @GetMapping
+    public String test() {
+        return "view/test";
     }
 }
