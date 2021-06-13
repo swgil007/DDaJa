048e1108789c72e6fd638e1ed0aa0562f9a2f893
swgil007 <swgil007@naver.com>
2021-06-14 08:13:24 +0900

    [#19] added Swagger Config

---
+++ BackEnd/ddaja/src/main/java/com/ddaja/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.ddaja.config;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ddaja.ddaja.controller"))
+                .paths(PathSelectors.ant("/**"))
+                .build();
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("API")
+                .description("[DDaJa] REST API")
+                .contact(new Contact("[DDaja Swagger]", "https://github.com/swgil007/DDaJa"))
+                .version("1.0")
+                .build();
+    }
+}
