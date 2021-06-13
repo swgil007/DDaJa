6e9374edb9f8b59c433524f6e32b50ce675985c2
swgil007 <swgil007@naver.com>
2021-06-14 08:19:19 +0900

    [#19] Update Swagger Config

--- BackEnd/ddaja/src/main/java/com/ddaja/config/SwaggerConfig.java
+++ BackEnd/ddaja/src/main/java/com/ddaja/config/SwaggerConfig.java
@@ -6,10 +6,18 @@ import java.util.Set;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -39,7 +47,7 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
                 .title("API")
                 .description("[DDaJa] REST API")
-                .contact(new Contact("[DDaja Swagger]", "https://github.com/swgil007/DDaJa"))
+                .contact(new Contact("[DDaja Swagger]", "https://github.com/swgil007/DDaJa", "BNG"))
                 .version("1.0")
                 .build();
     }
