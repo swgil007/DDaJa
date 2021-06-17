b59ac105293f14d7204f5bd3cd3e29b8e16b0425
swgil007 <swgil007@naver.com>
2021-06-17 09:10:16 +0900

    [#25] update Admin User Manage UI not Working table row data to popup

---
+++ vue/src/views/admin/user/component/components/ArticleDetail.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="createPost-container">
+    <el-form ref="postForm" :model="postForm" :rules="rules" class="form-container">
+      <div class="createPost-main-container">
+        <h1>회원 정보 수정1</h1>
+        <el-row>
+          <el-col
+            :span="24"
+            prop="userId"
+          />
+        </el-row>
+        <el-row>
+          <el-col :span="24">
+            <div class="choice-box">
+              <el-radio v-model="radio1" label="1" border>노출</el-radio>
+              <el-radio v-model="radio1" label="2" border>비 노출</el-radio>
+            </div>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="24" align="right">
+            <el-button @click="popupClose(false)">저장 하기</el-button>
+          </el-col>
+        </el-row>
+      </div>
+    </el-form>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+@import "~@/styles/mixin.scss";
+
+.createPost-container {
+  position: relative;
+
+  .createPost-main-container {
+    padding: 40px 45px 20px 50px;
+
+    .postInfo-container {
+      position: relative;
+      @include clearfix;
+      margin-bottom: 10px;
+
+      .postInfo-container-item {
+        float: left;
+      }
+    }
+  }
+
+  .word-counter {
+    width: 40px;
+    position: absolute;
+    right: 10px;
+    top: 0px;
+  }
+}
+
+.article-textarea ::v-deep {
+  textarea {
+    padding-right: 40px;
+    resize: none;
+    border: none;
+    border-radius: 0px;
+    border-bottom: 1px solid #bfcbd9;
+  }
+}
+
+.choice-box{
+    padding: 0 5.5% 1.5% 0;
+    float: right;
+}
+</style>
--- vue/src/views/admin/user/index.vue
+++ vue/src/views/admin/user/index.vue
@@ -1,94 +1,148 @@
 <template>
-  <el-table
-    :data="tableData"
-    style="width: 100%"
-  >
-    <el-table-column
-      fixed
-      prop="date"
-      label="가입일"
-      width="150"
+  <div class="main-container">
+    <div class="main-title">
+      <div class="div-1">
+        <font class="title-font1">사용자 관리</font>
+      </div>
+      <div class="div-2">
+        <el-button type="success" class="btn1" @click="popupState(true)">공지사항 작성</el-button>
+      </div>
+    </div>
+    <div class="table-box">
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+      >
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <p class="title">{{ props.row.title }}</p>
+            <p class="content">{{ props.row.content }}</p>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="제목"
+          prop="title"
+        />
+        <el-table-column
+          label="작성자"
+          prop="writer"
+        />
+        <el-table-column
+          label="작성일"
+          prop="date"
+        />
+        <el-table-column>
+          <template slot-scope="props">
+            <el-button
+              size="mini"
+              @click="popupState(props.row.title, props.row.content, true)"
+            >상세 보기</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="paging-box">
+      <el-pagination
+        class="paging"
+        layout="prev, pager, next"
+        :total="50"
+      />
+    </div>
+    <popup
+      :popup-val="popupVal"
+      @close:popup="popupState"
     />
-    <el-table-column
-      prop="name"
-      label="아이디"
-      width="120"
-    />
-    <el-table-column
-      prop="state"
-      label="닉네임"
-      width="120"
-    />
-    <el-table-column
-      prop="city"
-      label="상태"
-      width="120"
-    />
-    <el-table-column
-      prop="address"
-      label="Address"
-      width="300"
-    />
-    <el-table-column
-      prop="zip"
-      label="Zip"
-      width="120"
-    />
-    <el-table-column
-      fixed="right"
-      label="관리"
-      width="120"
-    >
-      <template>
-        <el-button type="text" size="middle" @click="handleClick">관리</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  </div>
 </template>
 
 <script>
+import popup from './component/userPopup.vue'
 export default {
+  name: '',
+  components: {
+    popup
+  },
   data() {
     return {
-      tableData: [{
-        date: '2016-05-03',
-        name: 'Tom',
-        state: 'California',
-        city: 'Los Angeles',
-        address: 'No. 189, Grove St, Los Angeles',
-        zip: 'CA 90036',
-        tag: 'Home'
-      }, {
-        date: '2016-05-02',
-        name: 'Tom',
-        state: 'California',
-        city: 'Los Angeles',
-        address: 'No. 189, Grove St, Los Angeles',
-        zip: 'CA 90036',
-        tag: 'Office'
-      }, {
-        date: '2016-05-04',
-        name: 'Tom',
-        state: 'California',
-        city: 'Los Angeles',
-        address: 'No. 189, Grove St, Los Angeles',
-        zip: 'CA 90036',
-        tag: 'Home'
-      }, {
-        date: '2016-05-01',
-        name: 'Tom',
-        state: 'California',
-        city: 'Los Angeles',
-        address: 'No. 189, Grove St, Los Angeles',
-        zip: 'CA 90036',
-        tag: 'Office'
-      }]
+      tableData: [
+        { date: '2021-06-11', writer: '길', title: '공지사항 1번 제목', content: '공지사항 1' },
+        { date: '2016-06-02', writer: '빈', title: '공지사항 2번 제목', content: '공지사항 3' },
+        { date: '2016-06-01', writer: '길', title: '공지사항 3번 제목', content: '공지사항 4' },
+        { date: '2016-06-03', writer: '빈', title: '공지사항 4번 제목', content: '공지사항 2' },
+        { date: '2016-06-04', writer: '길', title: '공지사항 5번 제목', content: '공지사항 5' }
+      ],
+      popupVal: false
     }
-  },
-  methods: {
-    handleClick() {
-      alert('관리할게요')
+  }, methods: {
+    popupState(id, name, value) {
+      this.popupVal = value
+      this.userId = id
+      this.userName = name
     }
   }
+
 }
 </script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Do+Hyeon&family=Jua&display=swap');
+.main-container{
+    width: 100%;
+    text-align: center;
+    padding: 30px;
+    overflow:auto;
+    background-color: rgb(255, 255, 255);
+    .main-title{
+        text-align: left;
+        margin-top: 5px;
+        margin-bottom: 30px;
+        color: rgb(0, 0, 0);
+        margin-left: 1%;
+        margin-right: 1%;
+        padding: 1.5% 2% 0% 4%;
+        .div-1{
+            width: 80%;
+            float: left;
+            .title-font1{
+                font-size: 50px;
+                font-family: 'Do Hyeon', sans-serif;
+            }
+            .title-font2{
+                font-size: 40px;
+                padding: 0 0 0 30px;
+                font-family: 'Do Hyeon', sans-serif;
+            }
+        }
+        .div-2{
+            width: 20%;
+            float: left;
+            .btn1{
+                float: right;
+                height: 50px;
+                width: 200px;
+            }
+        }
+    }
+    .table-box{
+        padding: 1.5% 2% 0% 4%;
+        margin: 0 0 30px 0 ;
+        float:left;
+        width: 100%;
+        max-height:550px;
+        .title{ font-size: 20px; font-weight: bold; }
+        .scrore{ font-size: 14px; font-weight: bold; }
+        .content{ padding: 1.5%; font-size: 14px; }
+    }
+    .paging-box{
+        float:left;
+        padding: 2% 11% 2% 0;
+        text-align: center;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        .paging{
+            display: inline-block;
+        }
+    }
+}
+</style>
