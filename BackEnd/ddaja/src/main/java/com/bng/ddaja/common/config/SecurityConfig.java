49bf84f289f207344498e88730ba28262e7f5769
swgil007 <swgil007@naver.com>
2021-06-26 08:14:06 +0900

    [#27] updated config package

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/SecurityConfig.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.bng.config;
+package com.bng.ddaja.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/SwaggerConfig.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.bng.config;
+package com.bng.ddaja.common.config;
 
 import java.util.HashSet;
 import java.util.Set;
