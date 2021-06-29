cc000d38830db355679c1b0a7c8148d29cdb5bbd
swgil007 <swgil007@naver.com>
2021-06-29 09:14:30 +0900

    [#27] change swagger request handler selectors base package to changed controller structure path

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/SwaggerConfig.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/SwaggerConfig.java
@@ -5,9 +5,6 @@ import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -27,7 +24,7 @@ public class SwaggerConfig{
                 .produces(getProduceContentTypes())
                 .apiInfo(getApiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ddaja.ddaja.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.bng.ddaja"))
                 .paths(PathSelectors.ant("/**"))
                 .build();
     }
