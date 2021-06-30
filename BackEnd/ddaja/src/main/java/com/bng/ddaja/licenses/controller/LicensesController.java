45c9479c2433d7ff477b5b1cf8e55198adce82a5
swgil007 <swgil007@naver.com>
2021-07-01 08:30:40 +0900

    [#27] added licenses collection structure

---
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/licenses/controller/LicensesController.java
@@ -0,0 +1,13 @@
+package com.bng.ddaja.licenses.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@RequestMapping("/licenses/*")
+@AllArgsConstructor
+public class LicensesController {
+    
+}
