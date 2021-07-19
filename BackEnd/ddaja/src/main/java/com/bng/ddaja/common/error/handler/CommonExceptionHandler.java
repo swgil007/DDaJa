6a2ca87d0b82c4d00c50397ede199574593dc13e
swgil007 <swgil007@naver.com>
2021-07-19 19:16:22 +0900

    [#33] added dependency javax.validation so plz do maven update & install that commits also fixed swgger configuration with OAS_30 chaning

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/error/handler/CommonExceptionHandler.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/error/handler/CommonExceptionHandler.java
@@ -1,17 +1,21 @@
 package com.bng.ddaja.common.error.handler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.bng.ddaja.common.dto.CommonError;
 import com.bng.ddaja.common.error.enums.ErrorCode;
 import com.bng.ddaja.common.error.exception.MemberNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class CommonExceptionHandler extends ResponseEntityExceptionHandler {
+public class CommonExceptionHandler {
     
     // @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     // public ResponseEntity<CommonError> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
@@ -23,5 +27,13 @@ public class CommonExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<CommonError> handleMemberNotFoundException(MemberNotFoundException e) {
         CommonError error = new CommonError(ErrorCode.NOT_FOUND_MEMBER);
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex){
+        // Map<String, String> errors = new HashMap<>();
+        // ex.getBindingResult().getAllErrors()
+        //         .forEach(c -> errors.put(c.getObjectName(), c.getDefaultMessage()));
+        return ResponseEntity.badRequest().body("Valid Error");
     }
 }
--- BackEnd/ddaja/src/main/java/com/bng/ddaja/test/controller/TestController.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/test/controller/TestController.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import com.bng.ddaja.common.dto.CommonError;
 import com.bng.ddaja.common.dto.CommonErrorDetail;
 import com.bng.ddaja.common.dto.CommonParameter;
@@ -15,6 +17,7 @@ import com.bng.ddaja.common.error.exception.MemberNotFoundException;
 import com.bng.ddaja.common.hateos.licenses.Licenses;
 import com.bng.ddaja.example.dto.UserDTO;
 import com.bng.ddaja.example.service.UserService;
+import com.bng.ddaja.test.dto.TestParameter;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +25,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.AllArgsConstructor;
 
-@Controller
+@RestController
 @RequestMapping("test")
 @AllArgsConstructor
 public class TestController {
@@ -35,37 +41,31 @@ public class TestController {
     private UserService userService;
     
     @GetMapping("")
-    @ResponseBody
     public String test() {
         return "test";
     }
 
     @GetMapping("/one/{id}")
-    @ResponseBody
     public List<Link> one(@PathVariable(name = "id") Long id) {
         return Licenses.SELF.makeLinkList(id);
     }
 
     @GetMapping("/two/{id}")
-    @ResponseBody
     public List<Link> two(@PathVariable(name = "id") Long id) {
         return Arrays.stream(Licenses.values()).map(e -> new Link(e.name(), e.url+id.toString(), e.method)).collect(Collectors.toList());
     }
 
     @GetMapping("/three")
-    @ResponseBody
     public CommonResponse<UserDTO> three(CommonParameter parameter) {
         return userService.findUserListByParameter(parameter);        
     }
 
     @GetMapping("/four")
-    @ResponseBody
     public CommonParameter four(CommonParameter parameter) {
         return parameter;
     }
 
     @GetMapping("/five")
-    @ResponseBody
     public ResponseEntity<CommonResponse<UserDTO>> five() {
         UserDTO userDTO = new UserDTO(Integer.toUnsignedLong(11), "test", "김텟트");
         CommonResponse<UserDTO> response = new CommonResponse<UserDTO>(1, new CommonResource<UserDTO>(userDTO, null));
@@ -73,7 +73,6 @@ public class TestController {
     }
 
     @GetMapping("/six")
-    @ResponseBody
     public ResponseEntity<CommonError> six() {
         List<CommonErrorDetail> errorDetailList = new LinkedList<>();
         errorDetailList.add(new CommonErrorDetail("parameter", "pqge=1", "notCorrectParam"));
@@ -82,8 +81,25 @@ public class TestController {
     }
 
     @GetMapping("/seven")
-    @ResponseBody
     public String seven() throws MemberNotFoundException {
         throw new MemberNotFoundException();
+    }
+
+
+    @PostMapping("/eight")
+    public String eight() {
+        return "tsdggsgs";
+    }
+
+
+    @PostMapping("/ten")
+    public String ten(@Valid @RequestBody TestParameter parameter ) {
+        return "ResponseEntity.ok().body(body success);";
+    }
+
+
+    @GetMapping("/nine")
+    public String nine() {
+        return "dfdfsdsssdf";
     }
 }
