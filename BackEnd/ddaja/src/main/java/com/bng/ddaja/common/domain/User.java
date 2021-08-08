fcf6baed247d874128bd5166db7d8e863947fd3a
swgil007 <swgil007@naver.com>
2021-08-08 18:51:20 +0900

    [#31] update UserEntity

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/domain/User.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/domain/User.java
@@ -2,22 +2,24 @@ package com.bng.ddaja.common.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.bng.ddaja.common.enums.TokenType;
+
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @ToString
-@EqualsAndHashCode
+@EqualsAndHashCode(callSuper = false, of = "id")
 @Table(name = "TB_USER")
 @NoArgsConstructor
 @Entity
@@ -25,14 +27,27 @@ public class User extends CommonEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="U_ID")
+    @Column(name = "U_ID")
     private long id;
     
-    @Column(name="ID")
+    @Column(name = "ID")
     private String userId;
 
-    @Column(name="NICK_NAME")
+    @Column(name = "NICK_NAME")
     private String nickName;
+
+    @Column(name = "EMAIL")
+    private String eMail;
+
+    @Column(name = "TOKEN_OAUTH")
+    private String tokenOAuth;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "TOKEN_TYPE")
+    private TokenType tokenType;
+
+    @Column(name = "PROFILE_IMG")
+    private String profileImg;
 
     @Builder
     public User(long id, String nickName) {
