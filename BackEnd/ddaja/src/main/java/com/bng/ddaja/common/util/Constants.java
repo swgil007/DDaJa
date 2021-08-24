1820605092f1cd95df43de6b16562410560fbd25
swgil007 <swgil007@naver.com>
2021-08-25 06:33:08 +0900

    [#37] added Constants Class

---
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/util/Constants.java
@@ -0,0 +1,12 @@
+package com.bng.ddaja.common.util;
+
+public final class Constants {
+    public static final String CLAIMS_ROLE = "role";
+    public static final String CLAIMS_USER_NAME = "userName";
+    public static final String CLAIMS_USER_ID = "userID";
+    public static final String CLAIMS_ID = "id";
+    public static final String ROLE_ADMIN = "admin";
+    public static final String ROLE_USER = "user";
+    public static final String ROLE_VISITOR = "visitor";
+    private Constants() {}
+}
