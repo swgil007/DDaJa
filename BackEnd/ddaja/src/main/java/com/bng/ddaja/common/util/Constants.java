a5ac4d96baaf285bb30441bb2000a75115e6e7e9
swgil007 <swgil007@naver.com>
2021-08-31 06:38:09 +0900

    [#37] added enums http methods

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/util/Constants.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/util/Constants.java
@@ -1,7 +1,7 @@
 package com.bng.ddaja.common.util;
 
 public final class Constants {
-    
+
     public static final String CLAIMS_ROLE = "role";
     public static final String CLAIMS_USER_NAME = "userName";
     public static final String CLAIMS_USER_ID = "userID";
@@ -11,12 +11,6 @@ public final class Constants {
     public static final String ROLE_VISITOR = "visitor";
     public static final String DATE_ISSUE = "iat";
     public static final String DATE_EXPIRE = "exp";
-    public static final String POST = "post";
-    public static final String GET = "get";
-    public static final String PUT = "put";
-    public static final String PATCH = "patch";
-    public static final String DELETE = "delete";
-    public static final String OPTIONS = "options";
 
     private Constants() {}
 }
