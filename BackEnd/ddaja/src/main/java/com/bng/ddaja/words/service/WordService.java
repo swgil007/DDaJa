66531e926a8d2a2a12e43bfb88653fdab54ded08
soobinJung <soobin7999@naver.com>
2021-09-04 22:00:13 +0900

    [#34] added get word list api

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/words/service/WordService.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/words/service/WordService.java
@@ -1,53 +1,33 @@
 package com.bng.ddaja.words.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.bng.ddaja.common.domain.License;
 import com.bng.ddaja.common.domain.Word;
-import com.bng.ddaja.temp.license.TempLicensesService;
 import com.bng.ddaja.words.dto.WordDTO;
+import com.bng.ddaja.words.dto.WordSearch;
 import com.bng.ddaja.words.repository.WordRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class WordService {
 
-    private WordRepository repository;
-
-    @Autowired
-    private TempLicensesService licenses;
-
-    /** WORD LIST - ALL **/
-    public List<WordDTO> findAll( ) { 
-        return repository.findAll().stream().map(vo -> new WordDTO(vo)).collect(Collectors.toList());
-    } 
+    private WordRepository wordRepository;
 
     /** WORD LIST - LICENSES **/
-    public List<WordDTO> wordLicenseList( long lid ) { 
-
-        List<WordDTO> list = new ArrayList<>();
-
-        License vo = License.builder().id( lid ).build(); 
-
-        repository.findByLicense( vo ).forEach( x -> { 
-            list.add( new WordDTO(x, licenses.findById(3)));
-        }); 
-        return list;
+    public Page<WordDTO> getAllWordByWordSearch( WordSearch wordSearch ) { 
+        return wordRepository.findAll(wordSearch.toSpecification(), wordSearch.toPageable()).map( vo -> new WordDTO(vo));
     } 
     
+
     /** WORD INSERT **/
     @Transactional
-    public void wordInsert ( Word vo ){ 
-        repository.save(vo);
+    public void createWord ( Word vo ){ 
+        wordRepository.save(vo);
     }
 }
--- BackEnd/ddaja/src/main/java/com/bng/ddaja/words/spec/WordSpec.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/words/spec/WordSpec.java
@@ -2,10 +2,13 @@ package com.bng.ddaja.words.spec;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-import com.bng.ddaja.common.domain.Word; 
+import com.bng.ddaja.common.domain.Word;
+import com.bng.ddaja.common.domain.License;
 
 import org.springframework.data.jpa.domain.Specification;
 
@@ -24,7 +27,8 @@ public class WordSpec {
         return new Specification<Word>(){
             public Predicate toPredicate(Root<Word> root, CriteriaQuery<?> query, CriteriaBuilder builder ){
                 if(licenseID == 0) return builder.conjunction();
-                return builder.equal(root.get("l_id"), licenseID);
+                Join<Word, License> j = root.join("license", JoinType.INNER);
+                return builder.equal(j.get("id"), licenseID);
             }
         };
     }
