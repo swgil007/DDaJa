90ae619bddd29f7d8b2fb6a42630b3dcc412b60d
swgil007 <swgil007@naver.com>
2021-09-04 15:54:07 +0900

    [#5] delete not used commit

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/error/handler/CommonExceptionHandler.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/error/handler/CommonExceptionHandler.java
@@ -15,22 +15,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import io.jsonwebtoken.JwtException;
 
 @RestControllerAdvice
 public class CommonExceptionHandler {
-    
-    // @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    // public ResponseEntity<CommonError> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
-    //     CommonError error = new CommonError(HttpStatus.METHOD_NOT_ALLOWED.value(), "Not Supported Method :(", null);
-    //     return new ResponseEntity<>(error, HttpStatus.METHOD_NOT_ALLOWED);
-    // }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex){
--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/domain/User.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/domain/User.java
@@ -4,15 +4,11 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.bng.ddaja.common.enums.TokenType;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
