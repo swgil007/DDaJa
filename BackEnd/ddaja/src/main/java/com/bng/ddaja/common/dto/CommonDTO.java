49a5b91a3f598a7c1b55d5f2c326f33711f80b7a
swgil007 <swgil007@naver.com>
2021-09-07 07:49:43 +0900

    [#29] added JsonInclude NonNull in CommonDTO

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/dto/CommonDTO.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/dto/CommonDTO.java
@@ -2,10 +2,14 @@ package com.bng.ddaja.common.dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+@JsonInclude(Include.NON_NULL)
 @Getter
 @Setter
 @ToString
