d1cb55bf9cffe11fa982a90da05b84172a4ff74f
swgil007 <swgil007@naver.com>
2021-09-29 08:46:59 +0900

    [#42] update mypage vue user info form

--- vue/src/views/mypage/components/userInfoForm.vue
+++ vue/src/views/mypage/components/userInfoForm.vue
@@ -1,42 +1,27 @@
 <template>
   <el-form v-show="isUserInfoForm" ref="form" :model="form" label-width="120px">
-    <el-form-item label="Activity name">
-      <el-input v-model="form.name" />
-    </el-form-item>
-    <el-form-item label="Activity zone">
-      <el-select v-model="form.region" placeholder="please select your zone">
-        <el-option label="Zone one" value="shanghai" />
-        <el-option label="Zone two" value="beijing" />
-      </el-select>
-    </el-form-item>
-    <el-form-item label="Activity time">
+    <el-form-item label="닉네임">
       <el-col :span="11">
-        <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;" />
-      </el-col>
-      <el-col class="line" :span="2">-</el-col>
-      <el-col :span="11">
-        <el-time-picker v-model="form.date2" placeholder="Pick a time" style="width: 100%;" />
+        <el-input v-model="form.name" />
       </el-col>
     </el-form-item>
-    <el-form-item label="Instant delivery">
-      <el-switch v-model="form.delivery" />
+    <el-form-item label="프로필">
+      <el-col :span="11">
+        <el-upload
+          class="avatar-uploader"
+          action="http://localhost/user/"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+        >
+          <img v-if="imageUrl" :src="imageUrl" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon" />
+        </el-upload>
+      </el-col>
     </el-form-item>
-    <el-form-item label="Activity type">
-      <el-checkbox-group v-model="form.type">
-        <el-checkbox label="Online activities" name="type" />
-        <el-checkbox label="Promotion activities" name="type" />
-        <el-checkbox label="Offline activities" name="type" />
-        <el-checkbox label="Simple brand exposure" name="type" />
-      </el-checkbox-group>
-    </el-form-item>
-    <el-form-item label="Resources">
-      <el-radio-group v-model="form.resource">
-        <el-radio label="Sponsor" />
-        <el-radio label="Venue" />
-      </el-radio-group>
-    </el-form-item>
-    <el-form-item label="Activity form">
-      <el-input v-model="form.desc" type="textarea" />
+    <el-form-item label="가입일">
+      <el-col :span="11">
+        <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;" :disabled="true" />
+      </el-col>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="onSubmit">Create</el-button>
@@ -54,8 +39,9 @@ export default {
     return {
       form: {
         name: '',
+        date1: '2021-09-27',
+        imageUrl: '',
         region: '',
-        date1: '',
         date2: '',
         delivery: false,
         type: [],
@@ -67,7 +53,48 @@ export default {
   methods: {
     onSubmit() {
       console.log('submit!')
+    },
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw)
+      console.log(this.imageUrl)
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isJPG) {
+        this.$message.error('Avatar picture must be JPG format!')
+      }
+      if (!isLt2M) {
+        this.$message.error('Avatar picture size can not exceed 2MB!')
+      }
+      return isJPG && isLt2M
     }
   }
 }
 </script>
+<style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
