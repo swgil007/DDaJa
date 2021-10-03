1c9631df88a52fdab16da798e3830afd8a9e646c
swgil007 <swgil007@naver.com>
2021-10-03 14:36:24 +0900

    [#5] fix conflict in openAPI

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/domain/OpenAPI.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/domain/OpenAPI.java
@@ -1,7 +1,5 @@
 package com.bng.ddaja.common.domain;
 
-public package com.bng.ddaja.common.domain;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
