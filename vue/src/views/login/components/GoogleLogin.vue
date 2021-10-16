483d9e34be4124dc08b62dae2b0eaf499553f5a4
swgil007 <swgil007@naver.com>
2021-10-16 13:43:05 +0900

    [#42] add google login vue

--- vue/src/views/login/components/GoogleLogin.vue
+++ vue/src/views/login/components/GoogleLogin.vue
@@ -21,16 +21,16 @@ export default {
       //   response_type: "token",
       //   scope: "https://www.googleapis.com/auth/contacts.readonly",
     //   // });
-    //   const REQUEST_URI = this.AUTHORIZE_URI + "?" + "client_id=" + this.$googleClientID
     //   console.log(REQUEST_URI)
     //   this.$http.get(REQUEST_URI)
     //   .then(res => {
-    //       console.log(res)
+      //       console.log(res)
     //   })
     //   .catch(err => {
-    //       console.log(err)
+      //       console.log(err)
     //   })
-      window.open('https://accounts.google.com/o/oauth2/v2/auth', 'window_name', 'width=430,height=500,location=no,status=no,scrollbars=yes')
+      const REQUEST_URI = this.AUTHORIZE_URI + '?' + 'client_id=' + this.$googleClientID + '&redirect_uri=' + this.REDIRECT_URI + '&response_type=' + this.RESPONSE_TYPE
+      window.open(REQUEST_URI, 'Google Login', 'width=430,height=500,location=no,status=no,scrollbars=yes')
     }
   }
 }
