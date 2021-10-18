9a92c60fe00b12f3584475aa1809afc442f8b474
swgil007 <swgil007@naver.com>
2021-10-19 07:52:46 +0900

    [#44] set google oauth api that scope, response type not matched

--- vue/src/views/login/components/GoogleLogin.vue
+++ vue/src/views/login/components/GoogleLogin.vue
@@ -8,7 +8,7 @@ export default {
   data() {
     return {
       AUTHORIZE_URI: 'https://accounts.google.com/o/oauth2/v2/auth',
-      REDIRECT_URI: 'localhost:9527/social/login',
+      REDIRECT_URI: 'http://localhost:9527/social/login',
       RESPONSE_TYPE: 'token',
       SCOPE: 'https://www.googleapis.com/auth/contacts.readonly'
     }
@@ -29,7 +29,7 @@ export default {
     //   .catch(err => {
       //       console.log(err)
     //   })
-      const REQUEST_URI = this.AUTHORIZE_URI + '?' + 'client_id=' + this.$googleClientID + '&redirect_uri=' + this.REDIRECT_URI + '&response_type=' + this.RESPONSE_TYPE + '&scope=' +
+      const REQUEST_URI = this.AUTHORIZE_URI + '?' + 'client_id=' + this.$googleClientID + '&redirect_uri=' + this.REDIRECT_URI + '&response_type=' + this.RESPONSE_TYPE + '&scope=' + this.SCOPE
       window.open(REQUEST_URI, 'Google Login', 'width=430,height=500,location=no,status=no,scrollbars=yes')
     }
   }
