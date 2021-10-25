00fec253a07f4fca9fadd8cee233093cc6689c6b
swgil007 <swgil007@naver.com>
2021-10-26 08:30:49 +0900

    [#13] add Validation in adminDTO for loginAdmin api

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/admin/controller/AdminController.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/admin/controller/AdminController.java
@@ -1,7 +1,12 @@
 package com.bng.ddaja.admin.controller;
 
+import javax.validation.Valid;
+
+import com.bng.ddaja.admin.dto.AdminDTO;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 public class AdminController {
     
     @PostMapping("login")
-    public ResponseEntity<String> loginAdmin() {
+    public ResponseEntity<String> loginAdmin(@RequestBody @Valid AdminDTO adminDTO) {
         return ResponseEntity.ok().body("TEST");
     }
 }
