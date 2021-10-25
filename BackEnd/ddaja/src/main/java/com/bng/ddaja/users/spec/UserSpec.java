e746c9d12895a461c9c4f9420c0e185a2126e790
swgil007 <swgil007@naver.com>
2021-10-25 15:30:17 +0900

    [#43] add UserSpec with nickName, id

---
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/users/spec/UserSpec.java
@@ -0,0 +1,31 @@
+package com.bng.ddaja.users.spec;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import com.bng.ddaja.common.domain.User;
+
+import org.springframework.data.jpa.domain.Specification;
+
+public class UserSpec {
+    
+    public static Specification<User> nickNameLike(final String nickName) {
+        return new Specification<User>() {
+            public Predicate toPredicate(Root<User> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+                if (nickName == null || "".equals(nickName)) return builder.conjunction();
+                return builder.like(root.get("nickName"), new StringBuilder().append("%").append(nickName).append("%").toString());
+            }
+        };
+    }
+
+    public static Specification<User> idEqual (final long id) {
+        return new Specification<User>() {
+            public Predicate toPredicate(Root<User> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+                if (id == 0) return builder.conjunction();
+                return builder.equal(root.get("id"), id);
+            }
+        };
+    }
+}
