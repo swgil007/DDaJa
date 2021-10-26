4f50541f5eebdb0cb24c809160e0a1fc8c5b33a8
swgil007 <swgil007@naver.com>
2021-10-27 08:49:40 +0900

    [#13] add /admin/login api

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/admin/controller/AdminController.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/admin/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.bng.ddaja.admin.controller;
 import javax.validation.Valid;
 
 import com.bng.ddaja.admin.dto.AdminDTO;
+import com.bng.ddaja.admin.service.AdminService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,9 +19,11 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 @RestController
 public class AdminController {
+
+    private AdminService adminService;
     
     @PostMapping("login")
-    public ResponseEntity<String> loginAdmin(@RequestBody @Valid AdminDTO adminDTO) {
-        return ResponseEntity.ok().body("TEST");
+    public ResponseEntity<AdminDTO> loginAdmin(@RequestBody @Valid AdminDTO adminDTO) {
+        return ResponseEntity.ok().body(adminService.loginAdminByAdminDTO(adminDTO));
     }
 }
