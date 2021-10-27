4a57f0d66076136606e49c292be009a55e6ca644
swgil007 <swgil007@naver.com>
2021-10-28 07:14:11 +0900

    [#5] testcode for using cookie

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/admin/controller/AdminController.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/admin/controller/AdminController.java
@@ -1,5 +1,10 @@
 package com.bng.ddaja.admin.controller;
 
+import java.util.Arrays;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import com.bng.ddaja.admin.dto.AdminDTO;
@@ -23,7 +28,16 @@ public class AdminController {
     private AdminService adminService;
     
     @PostMapping("login")
-    public ResponseEntity<AdminDTO> loginAdmin(@RequestBody @Valid AdminDTO adminDTO) {
+    public ResponseEntity<AdminDTO> loginAdmin(@RequestBody @Valid AdminDTO adminDTO, HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = new Cookie("jwt", "gillog");
+        cookie.setMaxAge(7 * 24 * 60 * 60);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        Cookie[] cookies = request.getCookies();
+        Arrays.stream(cookies).forEach(c -> {
+            log.info(c.getName() + c.getValue());
+        });
         return ResponseEntity.ok().body(adminService.loginAdminByAdminDTO(adminDTO));
     }
 }
