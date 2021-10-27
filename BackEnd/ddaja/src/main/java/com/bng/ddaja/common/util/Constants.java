d556b62ac224fc6486d64c828f9842d935984556
swgil007 <swgil007@naver.com>
2021-10-28 05:49:47 +0900

    [#13] add isSuper claims in CommonConstants

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/util/Constants.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/util/Constants.java
@@ -6,6 +6,7 @@ public final class Constants {
     public static final String CLAIMS_USER_NAME = "userName";
     public static final String CLAIMS_USER_ID = "userID";
     public static final String CLAIMS_ID = "id";
+    public static final String CLAIMS_IS_SUPER = "isSuper";
     public static final String ROLE_ADMIN = "admin";
     public static final String ROLE_USER = "user";
     public static final String ROLE_VISITOR = "visitor";
