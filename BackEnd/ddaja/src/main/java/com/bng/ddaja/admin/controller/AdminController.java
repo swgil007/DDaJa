edb7a73eb035a692479d824fb2b7585b3a7c8801
swgil007 <swgil007@naver.com>
2021-10-28 08:24:50 +0900

    [#5] cookieUtil test

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/admin/controller/AdminController.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/admin/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.bng.ddaja.admin.controller;
 
 import java.util.Arrays;
 
+import javax.security.sasl.AuthenticationException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,7 @@ import javax.validation.Valid;
 
 import com.bng.ddaja.admin.dto.AdminDTO;
 import com.bng.ddaja.admin.service.AdminService;
+import com.bng.ddaja.common.util.CookieUtil;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,16 +30,9 @@ public class AdminController {
     private AdminService adminService;
     
     @PostMapping("login")
-    public ResponseEntity<AdminDTO> loginAdmin(@RequestBody @Valid AdminDTO adminDTO, HttpServletRequest request, HttpServletResponse response) {
-        Cookie cookie = new Cookie("jwt", "gillog");
-        cookie.setMaxAge(7 * 24 * 60 * 60);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        response.addCookie(cookie);
-        Cookie[] cookies = request.getCookies();
-        Arrays.stream(cookies).forEach(c -> {
-            log.info(c.getName() + c.getValue());
-        });
+    public ResponseEntity<AdminDTO> loginAdmin(@RequestBody @Valid AdminDTO adminDTO, HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        CookieUtil.setJwtCookieInResponse(response, "jwttest");
+        log.info("getTokenInCookie ==" + CookieUtil.getTokenInCookie(request));
         return ResponseEntity.ok().body(adminService.loginAdminByAdminDTO(adminDTO));
     }
 }
