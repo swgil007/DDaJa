0f56550176bb7a8586b85df368f01457613dcf15
swgil007 <swgil007@naver.com>
2021-11-10 07:57:58 +0900

    [#5] remove unuseful findBymethod in UserRepository

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/users/repository/UserRepository.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/users/repository/UserRepository.java
@@ -8,6 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
-    public User findById(long id);
-    public Optional<User> findOptionalUserById(long id);
 }
