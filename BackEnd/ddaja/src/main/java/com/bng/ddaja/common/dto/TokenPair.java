ee9c695cf1c6c1b2e76c5bf9c92dc9f757e2fc03
swgil007 <swgil007@naver.com>
2021-11-10 08:11:33 +0900

    [#53] add cookie setting logic in TokenPair DTO

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/dto/TokenPair.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/dto/TokenPair.java
@@ -1,7 +1,9 @@
 package com.bng.ddaja.common.dto;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
+import com.bng.ddaja.common.config.exception.exception.CookieNotFoundException;
 import com.bng.ddaja.common.enums.TokenType;
 
 import lombok.Getter;
@@ -21,5 +23,12 @@ public class TokenPair {
         TokenType.ACCESS.setCookieConfigure(accessCookie);
         refreshCookie = new Cookie(TokenType.REFRESH.getName(), refreshToken);
         TokenType.REFRESH.setCookieConfigure(refreshCookie);
+    }
+
+    public void setCookie(HttpServletResponse response) {
+        if (accessCookie == null || refreshCookie == null)
+            throw new CookieNotFoundException("설정할 쿠키 정보가 존재하지 않습니다.");
+        response.addCookie(accessCookie);
+        response.addCookie(refreshCookie);
     }
 }
