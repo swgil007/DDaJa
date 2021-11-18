1bd9533ef244823bad090284ba2d7de40a94eda7
swgil007 <swgil007@naver.com>
2021-11-19 07:37:28 +0900

    [#37] fix swagger not open error with removing enableWebMvc Annotation in WebConfig

--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/SwaggerConfig.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/SwaggerConfig.java
@@ -5,6 +5,9 @@ import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig  {
+public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
@@ -51,5 +54,5 @@ public class SwaggerConfig  {
                 .version("1.0")
                 .build();
     }
-
+    
 }
--- BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/WebConfig.java
+++ BackEnd/ddaja/src/main/java/com/bng/ddaja/common/config/WebConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     
     @Override
