dc7814febc73f38cdeb72224092a92bc08b0d0f4
swgil007 <swgil007@naver.com>
2021-11-22 07:40:27 +0900

    [#38] add axios setting in main.js

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -5,8 +5,14 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 
-Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost'
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'origin, access-control-allow-origin'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.timeout = 100000
+axios.defaults.withCredentials = true
+Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
 new Vue({
