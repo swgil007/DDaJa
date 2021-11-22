a119e2dc0da51846a2de623a094ad2000066045d
swgil007 <swgil007@naver.com>
2021-11-23 07:41:10 +0900

    [#5] init FrontEnd

---
+++ FrontEnd/src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost'
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'origin, access-control-allow-origin'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.timeout = 100000
+axios.defaults.withCredentials = true
+
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
